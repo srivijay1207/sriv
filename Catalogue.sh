182cb92de4adc669c4c14a456a7799b5443a1ed8
srivijay <srivijay1207@gmail.com>
2023-08-23 22:33:30 +0530

    catalog scripting

--- Catalogue.sh
+++ Catalogue.sh
@@ -31,8 +31,8 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 VALIDATE $? " Rpm installed" 
 yum install nodejs -y &>>$LOGFILE
 VALIDATE $? "yum install"
-# useradd roboshop 
-#mkdir /app
+useradd roboshop 
+mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 VALIDATE $? "downloading"
 cd /app &>>$LOGFILE
